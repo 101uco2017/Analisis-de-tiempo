06e427c6f18d72d262b97a6cb971b9304360d947
isa9906 <isa8.perez@hotmail.com>
2018-03-15 20:59:40 -0500

    graficas torres de hanoi

--- torres hanoi/torres hanoi.cpp
+++ torres hanoi/torres hanoi.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 #include <stdlib.h>
 #include <windows.h>
+#include<fstream>
 void hanoi(int num,char A,char C,char B);
 double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b);
 using namespace std;
 int main()
 {
+		 ofstream fichero;
+    	fichero.open("daticos.csv",ios::app);
         int n;
         char A,B,C;
  		LARGE_INTEGER t_ini, t_fin;
   		double secs;
-        cout<<"Los clavijas son A B C\n";
-        cout<<"Numero de discos: ";
-        cin>>n;
-        QueryPerformanceCounter(&t_ini); //Midiendo tiempos
-        hanoi(n,'A','C','B');
-        QueryPerformanceCounter(&t_fin); //Midiendo tiempos
-        secs = performancecounter_diff(&t_fin,&t_ini)*1000;
-		cout<<n<<" Tiempo total: "<<secs<<"\n";
+        //cout<<"Los clavijas son A B C\n";
+        //cout<<"Numero de discos: ";
+        //cin>>n;
+        n=2;
+        while (n<=40){
+		
+        	QueryPerformanceCounter(&t_ini); //Midiendo tiempos
+        	hanoi(n,'A','C','B');
+        	QueryPerformanceCounter(&t_fin); //Midiendo tiempos
+        	secs = performancecounter_diff(&t_fin,&t_ini)*1000;
+        	fichero<<n<<";"<<secs<<"\n";
+			cout<<n<<" Tiempo total: "<<secs<<"\n";
+			n++;
+		}
+		fichero.close();
     	return 0;     
 }
 void hanoi(int num,char A,char C,char B)
