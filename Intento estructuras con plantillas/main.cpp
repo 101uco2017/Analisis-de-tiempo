dbcf4bc7a906f5fda7945a8632326ca9c5601180
AndresArias09 <leonardoandres.arias09@gmail.com>
2018-03-23 14:21:19 -0500

    Terminando ejemplo Templates

--- Intento estructuras con plantillas/main.cpp
+++ Intento estructuras con plantillas/main.cpp
@@ -5,12 +5,33 @@
 
 template <class T>
 struct nodo{
-	T *interior [];
+	T *interior;
 };
+
 using namespace std;
 int main(int argc, char** argv) {
 	
 	nodo < nodo < nodo < nodo <char> > > >  biblioteca;
+	//Contenido de la pagina
+	char *palabras = "Introducción a la primera página";
+	char *palabras2 = "Introducción a la segunda página";
+	//Creación de la página 1
+	nodo<char> pagina;
+	pagina.interior = palabras;
+	//Creación página 2
+	nodo<char> pagina2;
+	pagina.interior = palabras2;
+	//Creación del capítulo
+	nodo<char> pages[] = {pagina,pagina2}; //Arreglo de páginas
+	nodo<nodo<char> > capitulo;
+	capitulo.interior = pages;
+	//Creación del libro
+	nodo<nodo<char> > chapters[] = {capitulo,capitulo};
+	nodo<nodo<nodo<char> > > libro;
+	libro.interior = chapters;
+	//Creación de biblioteca
+	nodo<nodo<nodo<char> > > books[] = {libro,libro};
+	biblioteca.interior = books;
 	return 0;
 }
 
