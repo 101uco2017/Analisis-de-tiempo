7799bc753e21795b863bf270c29a275f7a2e5acb
isa9906 <isa8.perez@hotmail.com>
2018-03-22 20:21:34 -0500

    estructura biblioteca

---
+++ Intento estructuras con plantillas/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+template <class T>
+struct nodo{
+	T *interior [];
+};
+using namespace std;
+int main(int argc, char** argv) {
+	
+	nodo < nodo < nodo < nodo <char> > > >  biblioteca;
+	return 0;
+}
+
